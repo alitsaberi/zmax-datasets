206ec54a7b2ec8848ec7b9a017854d514cad0eb3
Ali Saberi <ali.saberi96@gmail.com>
2024-09-02 17:09:01 +0200

    Add Karolinska dataset

---
+++ zmax_datasets/datasets/karolinska.py
@@ -0,0 +1,62 @@
+import logging
+from pathlib import Path
+
+from zmax_datasets import settings
+from zmax_datasets.datasets.base import (
+    ZMaxDataset,
+    ZMaxRecording,
+)
+from zmax_datasets.exports.usleep import (
+    ErrorHandling,
+    ExistingFileHandling,
+    USleepExportStrategy,
+)
+from zmax_datasets.utils.helpers import load_yaml_config
+from zmax_datasets.utils.logger import setup_logging
+
+logger = logging.getLogger(__name__)
+
+# TODO: all of these variables should be configurable
+_ZMAX_DIR_PATTERN = "SNZ_*/ZMax/*"
+_SLEEP_SCORING_FILE_NAME_PATTERN = "{subject_id} {session_id}_all_in_one_ZMax.txt"
+
+
+class Karolinska(ZMaxDataset):
+    def __init__(
+        self,
+        data_dir: Path | str,
+        scoring_dir: Path | str,
+        zmax_dir_pattern: str = _ZMAX_DIR_PATTERN,
+        hypnogram_mapping: dict[int, str] = settings.USLEEP[
+            "default_hypnogram_mapping"
+        ],
+    ):
+        super().__init__(data_dir, zmax_dir_pattern, hypnogram_mapping)
+        self._scoring_dir = Path(scoring_dir)
+
+    @classmethod
+    def _extract_ids_from_zmax_dir(cls, zmax_dir: Path) -> tuple[str, str]:
+        return zmax_dir.parent.parent.name, zmax_dir.name
+
+    def _get_sleep_scoring_file(self, recording: ZMaxRecording) -> Path:
+        return self._scoring_dir / _SLEEP_SCORING_FILE_NAME_PATTERN.format(
+            subject_id=recording.subject_id.replace("SNZ_", ""),
+            session_id=recording.session_id,
+        )
+
+
+if __name__ == "__main__":
+    setup_logging()
+    config_file = settings.CONFIG_DIR / "datasets.yaml"
+    config = load_yaml_config(config_file)
+    dataset = Karolinska(**config["datasets"]["karolinska"])
+    export_strategy = USleepExportStrategy(
+        data_types=["EEG R", "EEG L"],
+        data_type_labels={
+            "EEG L": "F7-Fpz",
+            "EEG R": "F8-Fpz",
+        },
+        existing_file_handling=ExistingFileHandling.OVERWRITE,
+        error_handling=ErrorHandling.SKIP,
+    )
+    export_strategy.export(dataset, Path("data/karolinska"))
