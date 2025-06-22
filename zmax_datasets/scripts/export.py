117b1c94e2d4b2ed07a1f8e70fe5c9f6a2d4b39f
Ali Saberi <ali.saberi96@gmail.com>
2025-06-22 14:42:37 +0200

    Add export script
    - Remove gitkeep files from logs and data
    - Move scripts folder to the package folder and add export script
    - Remove logging config file and add datasets config file to the settings

---
+++ zmax_datasets/scripts/export.py
@@ -0,0 +1,111 @@
+import argparse
+from pathlib import Path
+
+from loguru import logger
+
+from zmax_datasets import datasets, settings
+from zmax_datasets.datasets.base import DATA_TYPES, DataTypeMapping, ZMaxDataset
+from zmax_datasets.exports.usleep import (
+    ErrorHandling,
+    ExistingFileHandling,
+    USleepExportStrategy,
+)
+from zmax_datasets.settings import LOGS_DIR
+from zmax_datasets.utils.helpers import (
+    generate_timestamped_file_name,
+    get_class_by_name,
+    load_yaml_config,
+)
+from zmax_datasets.utils.logger import setup_logging
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Export datasets")
+    parser.add_argument("output_dir", help="Output directory for exports", type=Path)
+    parser.add_argument(
+        "--datasets", nargs="+", help="List of datasets to export", type=str
+    )
+    parser.add_argument("--channels", nargs="+", help="Channels to extract", type=str)
+    parser.add_argument(
+        "--rename-channels", nargs="+", help="New names for the channels", type=str
+    )
+    parser.add_argument(
+        "--resample", action="store_true", help="Whether to resample data"
+    )
+    parser.add_argument(
+        "--overwrite", action="store_true", help="Overwrite existing files"
+    )
+    parser.add_argument("--skip-errors", action="store_true", help="Skip errors")
+    return parser.parse_args()
+
+
+def _get_datasets(datasets_to_export: list[str]) -> list[ZMaxDataset]:
+    config = load_yaml_config(settings.DATASETS_CONFIG_FILE)
+    available_datasets = list(config.keys())
+    if invalid_datasets := set(datasets_to_export) - set(available_datasets):
+        raise ValueError(
+            f"Invalid dataset name: {invalid_datasets}. "
+            f"Available datasets: {available_datasets}"
+        )
+    return [
+        get_class_by_name(dataset_name, datasets, ZMaxDataset)(**config[dataset_name])
+        for dataset_name in datasets_to_export
+    ]
+
+
+def _get_data_mappings(
+    channels: list[str], rename_channels: list[str]
+) -> list[DataTypeMapping]:
+    if invalid_channels := set(channels) - set(DATA_TYPES):
+        raise ValueError(
+            f"Invalid channel name: {invalid_channels}. "
+            f"Available channels: {DATA_TYPES}"
+        )
+
+    rename_channels = rename_channels or channels
+
+    if len(channels) != len(rename_channels):
+        raise ValueError(
+            f"Number of channels and rename channels must be the same. "
+            f"Got {len(channels)} channels and {len(rename_channels)} rename channels."
+        )
+
+    return [
+        DataTypeMapping(rename_channel, [channel])
+        for channel, rename_channel in zip(channels, rename_channels, strict=True)
+    ]
+
+
+def main() -> None:
+    log_file = LOGS_DIR / generate_timestamped_file_name(Path(__file__).stem, "log")
+    setup_logging(log_file=log_file)
+    args = parse_arguments()
+
+    logger.info(f"Arguments: {args}")
+
+    datasets = _get_datasets(args.datasets)
+    data_mappings = _get_data_mappings(args.channels, args.rename_channels)
+
+    for dataset in datasets:
+        logger.info(f"Exporting dataset: {dataset.__class__.__name__}")
+
+        export_strategy = USleepExportStrategy(
+            data_type_mappigns=data_mappings,
+            sampling_frequency=settings.USLEEP["sampling_frequency"]
+            if args.resample
+            else settings.ZMAX["sampling_frequency"],
+            existing_file_handling=ExistingFileHandling.OVERWRITE
+            if args.overwrite
+            else ExistingFileHandling.APPEND,
+            error_handling=ErrorHandling.SKIP
+            if args.skip_errors
+            else ErrorHandling.RAISE,
+        )
+
+        export_strategy.export(
+            dataset, Path(args.output_dir) / dataset.__class__.__name__
+        )
+
+
+if __name__ == "__main__":
+    main()
--- zmax_datasets/utils/logger.py
+++ zmax_datasets/utils/logger.py
@@ -1,84 +1,55 @@
-import datetime as dt
-import json
 import logging.config
+import sys
+from enum import Enum
+from pathlib import Path
+from typing import Any
 
-from typing_extensions import override
+from loguru import logger
 
 from zmax_datasets import settings
-from zmax_datasets.utils.helpers import load_yaml_config
-
-LOG_RECORD_BUILTIN_ATTRS = {
-    "args",
-    "asctime",
-    "created",
-    "exc_info",
-    "exc_text",
-    "filename",
-    "funcName",
-    "levelname",
-    "levelno",
-    "lineno",
-    "module",
-    "msecs",
-    "message",
-    "msg",
-    "name",
-    "pathname",
-    "process",
-    "processName",
-    "relativeCreated",
-    "stack_info",
-    "thread",
-    "threadName",
-    "taskName",
-}
 
 
-class JSONFormatter(logging.Formatter):
-    def __init__(
-        self,
-        *,
-        fmt_keys: dict[str, str] | None = None,
-    ):
-        super().__init__()
-        self.fmt_keys = fmt_keys or {}
-
-    @override
-    def format(self, record: logging.LogRecord) -> str:
-        message = self._prepare_log_dict(record)
-        return json.dumps(message, default=str)
-
-    def _prepare_log_dict(self, record: logging.LogRecord):
-        always_fields = {
-            "message": record.getMessage(),
-            "timestamp": dt.datetime.fromtimestamp(
-                record.created, tz=dt.timezone.utc
-            ).isoformat(),
-        }
-        if record.exc_info is not None:
-            always_fields["exc_info"] = self.formatException(record.exc_info)
-
-        if record.stack_info is not None:
-            always_fields["stack_info"] = self.formatStack(record.stack_info)
-
-        message = {
-            key: msg_val
-            if (msg_val := always_fields.pop(val, None)) is not None
-            else getattr(record, val)
-            for key, val in self.fmt_keys.items()
-        }
-        message.update(always_fields)
-
-        for key, val in record.__dict__.items():
-            if key not in LOG_RECORD_BUILTIN_ATTRS:
-                message[key] = val
-
-        return message
+class HandlerType(Enum):
+    CONSOLE = "console"
+    FILE = "file"
 
 
-def setup_logging(file_name: str | None = settings.PACKAGE_NAME) -> None:
-    config = load_yaml_config(settings.LOGGING_CONFIG_FILE)
-    config["handlers"]["file"]["filename"] = (
-        settings.LOGS_DIR / f"{file_name}{settings.LOG_FILE_EXTENSION}"
-    )
-    logging.config.dictConfig(config)
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+def setup_logging(
+    log_file: Path = settings.LOGGING["log_file"],
+    config: dict[str, Any] = settings.LOGGING,
+) -> None:
+    logger.remove()
+
+    for handler_name, handler_config in config["handlers"].items():
+        handler_type = HandlerType(handler_name)
+
+        match handler_type:
+            case HandlerType.CONSOLE:
+                sink = sys.stdout
+            case HandlerType.FILE:
+                sink = log_file
+
+        logger.add(sink, **handler_config)
+
+    # Intercept standard library logging
+    # This way, all logs from the root logger and other libraries using standard
+    # logging will be properly formatted and handled by Loguru
+    logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
