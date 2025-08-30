a6cdae1ea7cd16021061918095b55434cb9ad7f4
Ali Saberi <ali.saberi96@gmail.com>
2025-08-30 14:05:07 +0200

    Return IBI from ProcessPPG transform and remove the ExtractIBI transformation class and related IBI processing scripts as they are no longer needed

--- zmax_datasets/transforms/ppg.py
+++ zmax_datasets/transforms/ppg.py
@@ -2,9 +2,7 @@ from enum import Enum
 
 import neurokit2 as nk
 import numpy as np
-from scipy.interpolate import interp1d
 
-from zmax_datasets.processing.ibi import extract_ibi_from_peaks
 from zmax_datasets.transforms.base import Transform
 from zmax_datasets.utils.data import Data
 
@@ -27,18 +25,33 @@ class DetrendMethod(Enum):
     LOCREG = "locreg"
 
 
+class InterpolationMethod(Enum):
+    LINEAR = "linear"
+    NEAREST = "nearest"
+    ZERO = "zero"
+    SLINEAR = "slinear"
+    QUADRATIC = "quadratic"
+    CUBIC = "cubic"
+    PREVIOUS = "previous"
+    NEXT = "next"
+    MONOTONE_CUBIC = "monotone_cubic"
+    AKIMA = "akima"
+
+
 class ProcessPPG(Transform):
-    CHANNEL_NAMES = ["peaks", "rate", "quality"]
+    CHANNEL_NAMES = ["peaks", "ibi", "rate", "quality"]
 
     def __init__(
         self,
         peak_detection_method: str = PeakDetectionMethod.ELGENDI,
         quality_method: str = QualityMethod.TEMPLATE_MATCH,
         correct_artifacts: bool = False,
+        interpolation_method: str = InterpolationMethod.MONOTONE_CUBIC,
     ):
         self.peak_detection_method = peak_detection_method
         self.quality_method = quality_method
         self.correct_artifacts = correct_artifacts
+        self.interpolation_method = interpolation_method
 
     def __call__(self, data: Data) -> Data:
         if data.n_channels != 1:
@@ -54,15 +67,17 @@ class ProcessPPG(Transform):
             sampling_rate=int(data.sample_rate),
             method=self.peak_detection_method,
             correct_artifacts=self.correct_artifacts,
-            show=False,
         )
 
-        # Rate computation
-        rate = nk.signal_rate(
+        periods = nk.signal_period(
             info["PPG_Peaks"],
             sampling_rate=int(data.sample_rate),
             desired_length=len(ppg_signal),
+            interpolation_method=self.interpolation_method.value,
         )
+
+        ibi = periods * 1000
+        rate = 60 / periods
 
         # Assess signal quality
         quality = nk.ppg_quality(
@@ -72,62 +87,11 @@ class ProcessPPG(Transform):
             method=self.quality_method,
         )
 
-        array = np.array([peaks["PPG_Peaks"].values, rate, quality]).T
+        array = np.array([peaks["PPG_Peaks"].values, ibi, rate, quality]).T
 
         return Data(
             array=array,
             sample_rate=data.sample_rate,
             timestamps=data.timestamps,
             channel_names=self.CHANNEL_NAMES,
-        )
-
-
-class ExtractIBI(Transform):
-    CHANNEL_NAMES = ["ibi"]
-
-    def __init__(
-        self,
-        interpolation_rate: float,
-        detrend: DetrendMethod | None = None,
-        match_length: bool = True,
-    ):
-        self.interpolation_rate = interpolation_rate
-        self.detrend = detrend
-        self.match_length = match_length
-
-    def __call__(self, peaks: Data, **kwargs) -> Data:
-        if peaks.n_channels != 1:
-            raise ValueError(
-                "PPG peaks data must have exactly one channel."
-                f" Found {peaks.n_channels} channels."
-            )
-
-        peaks_signal = peaks.array.squeeze()
-        ibi_values, ibi_times = extract_ibi_from_peaks(peaks_signal, peaks.sample_rate)
-
-        # Process IBI values
-        ibi_signal, ibi_times, _ = nk.intervals_process(
-            ibi_values,
-            intervals_time=ibi_times,
-            interpolate=True,
-            interpolation_rate=self.interpolation_rate,
-            detrend=self.detrend,
-            **kwargs,
-        )
-
-        # Ensure the length matches peaks by interpolating to exact timestamps
-        if self.match_length and len(ibi_signal) != len(peaks_signal):
-            f = interp1d(
-                ibi_times, ibi_signal, bounds_error=False, fill_value="extrapolate"
-            )
-
-            # Generate timestamps matching peaks length
-            target_times = np.arange(len(peaks_signal)) / peaks.sample_rate
-            ibi_signal = f(target_times)
-
-        return Data(
-            array=ibi_signal.reshape(-1, 1),
-            sample_rate=peaks.sample_rate,
-            timestamps=peaks.timestamps,
-            channel_names=["ibi"],
         )
